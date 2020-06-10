6e58fc3728c2b59c9047b11b731af6ca54bccd4c
RastogiAbhijeet <abhijeet.rastogi@block8.com>
2020-06-10 23:38:37 +0530

    Update the name of the repo

--- terraform/cloud_build_trigger.tf
+++ terraform/cloud_build_trigger.tf
@@ -2,7 +2,7 @@ resource "google_cloudbuild_trigger" "github_repo" {
   provider = google-beta
   github {
     owner = "RastogiAbhijeet"
-    name  = "spinnaker_tf_config"
+    name  = "terraform-spinnaker-helm"
 
     push {
       tag = "v.*"
